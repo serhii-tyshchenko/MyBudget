c255f827b67545f09e7a1a045ecd0e40169c7192
Serhii Tyshchenko (EPAM) <serhii_tyshchenko1@epam.com>
2022-01-28 23:48:27 +0200

    edit styles

--- src/components/molecules/DataListItem/DataListItem.tsx
+++ src/components/molecules/DataListItem/DataListItem.tsx
@@ -28,21 +28,11 @@ const DataListItem = ({ data, onDeleteClick, onEditClick }: TProps) => {
   return (
     <li className={NAME_SPACE}>
       <div className={`${NAME_SPACE}__data`}>
-        <span>
-          {STR.DATE}: {date}
-        </span>
-        <span>
-          {STR.ACCOUNT}: {account}
-        </span>
-        <span>
-          {STR.CATEGORY}: {category}
-        </span>
-        <span>
-          {STR.AMOUNT}: {amount}
-        </span>
-        <span>
-          {STR.NOTE}: {note}
-        </span>
+        <span className={`${NAME_SPACE}__date`}>{date}</span>
+        <span className={`${NAME_SPACE}__account`}>{account}</span>
+        <span className={`${NAME_SPACE}__category`}>{category}</span>
+        <span className={`${NAME_SPACE}__amount`}>{amount}</span>
+        <span className={`${NAME_SPACE}__note`}>{note}</span>
       </div>
       <div className={`${NAME_SPACE}__controls`}>
         <UIIconButton
@@ -59,5 +49,21 @@ const DataListItem = ({ data, onDeleteClick, onEditClick }: TProps) => {
     </li>
   );
 };
+const DataListItemHeader = () => {
+  const STR = useLocalization();
 
-export { DataListItem };
+  return (
+    <li className={`${NAME_SPACE} ${NAME_SPACE}--header`}>
+      <div className={`${NAME_SPACE}__data`}>
+        <span className={`${NAME_SPACE}__date`}>{STR.DATE}</span>
+        <span className={`${NAME_SPACE}__account`}>{STR.ACCOUNT}</span>
+        <span className={`${NAME_SPACE}__category`}>{STR.CATEGORY}</span>
+        <span className={`${NAME_SPACE}__amount`}>{STR.AMOUNT}</span>
+        <span className={`${NAME_SPACE}__note`}>{STR.NOTE}</span>
+      </div>
+      <div className={`${NAME_SPACE}__controls`} />
+    </li>
+  );
+};
+
+export { DataListItem, DataListItemHeader };
